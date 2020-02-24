a1fa5522600cfc6ae8beb10a4d87ae6de1b40e12
Dominic Feliton <118365@ecrchs.org>
2020-02-24 17:40:07 +0000

    Initial commit

---
+++ Number.java
@@ -0,0 +1,52 @@
+//Name: 
+//Date: 
+//Per: 
+//AP Comp Sci
+
+public class Number
+{
+	private Integer number;
+
+	public Number()
+	{
+    setNumber(0);
+	}
+
+	public Number(int num)
+	{
+    setNumber(num);
+	}
+
+	public void setNumber(int num)
+	{
+    number = num;
+	}
+
+	public int getNumber()
+	{
+		return number;
+	}
+
+	public boolean isOdd()
+	{
+    return (number % 2 == 1);
+	}
+
+	public boolean isPerfect()
+	{
+		int total=0;
+    for (int i = 1; i < number; i++)
+    {
+      if (number % i == 0)
+      {
+        total+=i;
+      }
+    }
+		return (number==total);
+	}
+	
+	public String toString( )
+	{
+		return "";
+	}
+}
---
+++ NumberAnalyzer.java
@@ -0,0 +1,62 @@
+//Name: 
+//Date: 
+//Per: 
+//AP Comp Sci
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import static java.lang.System.*;
+
+public class NumberAnalyzer
+{
+	private ArrayList<Number> list;
+
+	public NumberAnalyzer()
+	{
+    list = new ArrayList<Number>();
+	}
+
+	public NumberAnalyzer(String numbers)
+	{
+
+	}
+	
+	public void setList(String numbers)
+	{
+
+
+	
+	}
+
+	public int countOdds()
+	{
+      int oddCount=0;
+
+
+
+      return oddCount;
+	}
+
+	public int countEvens()
+	{
+      int evenCount=0;
+
+
+
+      return evenCount;
+	}
+
+	public int countPerfects()
+	{
+		int perfectCount=0;
+
+
+
+      return perfectCount;
+	}
+	
+	public String toString( )
+	{
+		return "";
+	}
+}
