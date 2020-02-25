62b4cb3b5fd0a91f8fd2846c436ab747287bab36
118365ecr <118365@ecrchs.org>
2020-02-25 06:24:15 +0000

    Everything works!!!

--- NumberAnalyzer.java
+++ NumberAnalyzer.java
@@ -4,6 +4,7 @@
 //AP Comp Sci
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import static java.lang.System.*;
 
@@ -14,34 +15,49 @@ public class NumberAnalyzer
 	public NumberAnalyzer()
 	{
 		list = new ArrayList<Number>();
+		setList("6 9 4 2 0");
 	}
 
 	public NumberAnalyzer(String numbers)
 	{
 		list = new ArrayList<Number>();
-		int spacePos = 0;
-		for (int i = 0; i < numbers.length(); i++)
-		{
-			spacePos = numbers.substring(i).indexOf(" ");
-			System.out.println(spacePos);
-			int currNum = Integer.parseInt(numbers.substring(i, spacePos));
-			Number num = new Number(currNum);
-			list.add(num);
-		}
+		setList(numbers);
 	}
 	
 	public void setList(String numbers)
 	{
-
-
-	
+		int spacePos = numbers.indexOf(" ");
+		String cutMeUp = numbers;
+		
+		for (int i = 0; i < numbers.length(); i++) //99% sure I'm a crackhead or smth
+		{
+			if (spacePos != -1)
+			{
+				int eaNum = Integer.parseInt(cutMeUp.substring(0, spacePos));
+				Number num = new Number(eaNum);
+				list.add(num);
+				cutMeUp = cutMeUp.substring(spacePos + 1);
+			}
+			else
+			{
+				break;
+			}
+			spacePos = cutMeUp.indexOf(" ");
+		}
+		Number finalNum = new Number(Integer.parseInt(cutMeUp));
+		list.add(finalNum); //garbage algorithm is garbage, this works how
 	}
 
 	public int countOdds()
 	{
       int oddCount=0;
-
-
+      for (Number eaNum : list)
+      {
+    	  if (eaNum.getNumber() % 2 == 1)
+    	  {
+    		  oddCount++;
+    	  }
+      }
 
       return oddCount;
 	}
@@ -49,27 +65,49 @@ public class NumberAnalyzer
 	public int countEvens()
 	{
       int evenCount=0;
-
-
+      for (Number eaNum : list)
+      {
+    	  if (eaNum.getNumber() % 2 == 0)
+    	  {
+    		  evenCount++;
+    	  }
+      }
 
       return evenCount;
 	}
 
 	public int countPerfects()
 	{
-		 int perfectCount=0;
-     for (int i = 0; i < list.length; i++)
-     {
-       if (list[i].isPerfect())
-       {
-         perfectCount++;
-       }
-     }
-      return perfectCount;
+		int perfectCount=0;
+		
+		//get divisors
+		int divisorTotal = 0;
+		for (Number eaNum: list)
+		{
+			for (int i = 1; i < eaNum.getNumber(); i++) //get divisors of this number
+			{
+				if (eaNum.getNumber() % i == 0)
+				{
+					divisorTotal+=i;
+				}
+			}
+			if (eaNum.getNumber() == divisorTotal) //check for perfect
+			{
+				perfectCount++; //add one if perfect
+			}
+			divisorTotal = 0; //reset val for next num
+		}
+		
+		return perfectCount;
 	}
 	
 	public String toString( )
 	{
-		return "";
+		int outArray[] = new int[list.size()];
+		for (int i = 0; i < outArray.length; i++)
+		{
+			outArray[i] = list.get(i).getNumber();
+		}
+		return Arrays.toString(outArray);
 	}
 }
